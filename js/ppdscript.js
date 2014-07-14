a6cba2b76e3e09919668e12f93cf2f1070da0a5c
telpuk <telpuk.sergey@gmail.com>
2014-07-14 15:49:15 +0300

    исправил пропуски

--- js/ppdscript.js
+++ js/ppdscript.js
@@ -1,5 +1,8 @@
-var test,
-    doc = document,
+var doc = document,
+    activeQuestionIndex,
+    rightQuestions = [],
+    wrongQuestions = [],
+    listTestName = doc.getElementsByClassName('namesTest')[0],
     hidden = doc.getElementsByClassName('hidden')[0],
     testList = doc.getElementsByClassName('testList')[0],
     titleTest = doc.getElementsByClassName('titleTest')[0],
@@ -10,24 +13,7 @@ var test,
     contentQuestions = doc.getElementsByClassName('content')[0],
     imgQuestions = doc.getElementsByClassName('imgQuestions')[0],
     listAnswers = doc.getElementsByClassName('listAnswers')[0],
-    skipAnswerButton = doc.getElementsByClassName('skipAnswerButton')[0],
-    activeQuestion,
-    rightQuestions = [],
-    wrongQuestions = [];
-
-//получение тестов
-var arrayTest = function() {
-    var array = [];
-    for (var testId in quizData) {
-        array[testId] = quizData[testId].title;
-    }
-    return array;
-}();
-
-//найти тест
-var setTest = function(testId) {
-    test = quizData[testId];
-};
+    skipAnswerButton = doc.getElementsByClassName('skipAnswerButton')[0];
 
 var displayShowRightQuestions = function(count) {
     countRightQuestions.innerText = count.length;
@@ -47,7 +33,7 @@ var createLisAnswers = function(listAnswers) {
         input.setAttribute('name', 'answer');
         input.setAttribute('value', id);
         //устанавливаем default
-        if (parseInt(id) === 0) {
+        if (parseInt(id, 10) === 0) {
             input.setAttribute('checked', 'true');
         }
         li.appendChild(input);
@@ -57,29 +43,35 @@ var createLisAnswers = function(listAnswers) {
     return ul;
 };
 //построить вопрос
-var buildQuestion = function(question) {
-    activeQuestion = question;
-    activeQuestions.innerText = activeQuestion.index + 1;
-    contentQuestions.innerText = question.content;
-    imgQuestions.appendChild(function(img) {
-        var imgtest = doc.createElement('IMG');
-        imgtest.src = img;
-        return imgtest;
-    }(question.img));
-    listAnswers.appendChild(createLisAnswers(question.answers));
+var buildQuestion = function(indexActive) {
+    activeQuestionIndex = indexActive;
+    activeQuestions.innerText = activeQuestionIndex + 1;
+    contentQuestions.innerText = quizData[indexActiveTest].questions[activeQuestionIndex].question;
+
+    if (quizData[indexActiveTest].questions[activeQuestionIndex].questionImg) {
+        imgQuestions.appendChild(function(img) {
+            var imgtest = doc.createElement('IMG');
+            imgtest.src = img;
+            return imgtest;
+        }(quizData[indexActiveTest].questions[activeQuestionIndex].questionImg));
+    }
+
+    listAnswers.appendChild(createLisAnswers(quizData[indexActiveTest].questions[activeQuestionIndex].answers));
 };
 
 //построить тест
-var buildTestWidget = function() {
-    titleTest.innerText = test.title;
-    question.innerText = test.questions.length;
+var buildTestWidget = function(indexActive) {
+    //установить активный тест
+    indexActiveTest = indexActive;
+    titleTest.innerText = quizData[indexActive].title;
+    question.innerText = quizData[indexActive].questions.length;
     displayShowRightQuestions(rightQuestions);
     displayShowWrongQuestions(wrongQuestions);
-    buildQuestion(test.questions[0]);
+    buildQuestion(0);
 };
 
 var repleceClass = function(element, nameClass) {
-    element.className = 'hidden';
+    element.className = nameClass;
 };
 
 var romeveClass = function(element, nameClass) {
@@ -88,103 +80,133 @@ var romeveClass = function(element, nameClass) {
 
 //создание элементов списка
 var createListTest = function() {
-    var listTestName = doc.getElementsByClassName('namesTest')[0];
     var ul = doc.createElement('UL');
     listTestName.appendChild(ul);
-    for (var testId in arrayTest) {
-        var text = doc.createTextNode(arrayTest[testId]);
+    for (var testId in quizData) {
+        var text = doc.createTextNode(quizData[testId].title);
         var li = doc.createElement('LI');
         var a = doc.createElement('A');
         a.setAttribute("href", '#');
-        //построение контента теста
-        a.addEventListener('click', function(x) {
+
+        a.addEventListener('click', function(index) {
             return function() {
-                setTest(x);
+                buildTestWidget(index);
             };
         }(testId));
-
-        a.addEventListener('click', buildTestWidget);
 
         a.addEventListener('click', function() {
             repleceClass(testList, 'hidden');
         });
         a.addEventListener('click', function() {
-            romeveClass(hidden, 'hidden');
+            repleceClass(hidden, 'hiddenShowDisplay');
         });
         a.appendChild(text);
         li.appendChild(a);
         ul.appendChild(li);
     }
 
-}();
+};
 
 var deleteQuestion = function() {
     contentQuestions.removeChild(contentQuestions.firstChild);
-    imgQuestions.removeChild(imgQuestions.firstChild);
+    //проверка на картику
+    if (imgQuestions.firstChild) {
+        imgQuestions.removeChild(imgQuestions.firstChild);
+    }
+
     listAnswers.removeChild(listAnswers.firstChild);
 
 };
 
 var getIndex = function(idx) {
     var countFlag = 0;
-    ++idx;
+    var countQuestions = quizData[indexActiveTest].questions.length;
 
-    for (var id in test.questions) {
-        if (test.questions[id].flag) {
+    for (var id in quizData[indexActiveTest].questions) {
+        if (quizData[indexActiveTest].questions[id].flag) {
             ++countFlag;
         }
     }
-    if (countFlag === test.questions.length) {
+
+    if (countFlag === countQuestions) {
+        skipAnswerButton.style.display = 'none';
         return false;
-    } else if (countFlag === test.questions.length - 1) {
-        repleceClass(skipAnswerButton, 'hidden');
-        return idx;
-    }
-    else {
-        idx = idx > test.questions[test.questions.length - 1].index ? 0 : idx;
-        while (test.questions[idx].flag) {
-            idx = ++idx > test.questions[test.questions.length - 1].index ? 0 : idx;
+    } else {
+        if (countFlag === countQuestions - 1) {
+            skipAnswerButton.style.display = 'none';
         }
-
+        idx = ++idx > (countQuestions - 1) ? 0 : idx;
+        while (quizData[indexActiveTest].questions[idx].flag) {
+            idx = ++idx > (countQuestions - 1) ? 0 : idx;
+        }
         return idx;
     }
 
+};
+
+var resetFlagsANDanswers = function() {
+    rightQuestions = [];
+    wrongQuestions = [];
+    for (var id in quizData[indexActiveTest].questions) {
+        quizData[indexActiveTest].questions[id].flag = false;
+    }
+};
+var setFlagPassedTest = function() {
+    var indexTest = quizData.indexOf(quizData[indexActiveTest]);
+    var passedTest = listTestName.getElementsByTagName('A')[indexTest];
+    passedTest.innerText += " + ";
 };
 
 var clikNextButton = function() {
     var inputListRadio = listAnswers.getElementsByTagName('input');
     for (var idInput = 0; idInput < inputListRadio.length; ++idInput) {
         if (inputListRadio[idInput].checked) {
-            var answer = inputListRadio[idInput].value;
-            if (parseInt(answer) === activeQuestion.right) {
-                rightQuestions.push(activeQuestion);
+            var answer = parseInt(inputListRadio[idInput].value, 10);
+            if ((++answer) === parseInt(quizData[indexActiveTest].questions[activeQuestionIndex].right, 10)) {
+                rightQuestions.push(1);
                 displayShowRightQuestions(rightQuestions);
             } else {
-                wrongQuestions.push(activeQuestion);
+                wrongQuestions.push(1);
                 displayShowWrongQuestions(wrongQuestions);
-                alert(activeQuestion.wrongAnswersComment);
+                alert(quizData[indexActiveTest].questions[activeQuestionIndex].wrongAnswersComment);
             }
         }
     }
-    activeQuestion.flag = true;
-    var id = getIndex(activeQuestion.index);
+    quizData[indexActiveTest].questions[activeQuestionIndex].flag = true;
+    var id = getIndex(activeQuestionIndex);
     if (id >= 0 && id !== false) {
         deleteQuestion();
-        buildQuestion(test.questions[id], activeQuestion.index);
+        buildQuestion(id);
     } else {
-        repleceClass(skipAnswerButton, 'hidden');
         alert('Статистика');
+        repleceClass(doc.getElementsByClassName('hiddenShowDisplay')[0], 'hidden');
+        repleceClass(doc.getElementsByClassName('hidden')[0], 'hiddenShowDisplay');
+        skipAnswerButton.style.display = '';
+        deleteQuestion();
+        resetFlagsANDanswers();
+        setFlagPassedTest();
+
     }
 };
 
 var clikSkipButton = function() {
-    var id = getIndex(activeQuestion.index);
+    var id = getIndex(activeQuestionIndex);
     deleteQuestion();
-    buildQuestion(test.questions[id], activeQuestion.index);
+    buildQuestion(id);
 
 };
+(function() {
+    for (var idQuest in quizData) {
+        var idQuizData = quizData[idQuest].questions;
+        for (var i in idQuizData) {
+            idQuizData[i].flag = false;
+            idQuizData[i].wrongAnswersComment = "Не правильно отвечен вопрос!";
+        }
+    }
+})();
 
 (function() {
+    createListTest();
     doc.getElementsByClassName('answerButton')[0].addEventListener('click', clikNextButton);
     doc.getElementsByClassName('skipAnswerButton')[0].addEventListener('click', clikSkipButton);
 })();
