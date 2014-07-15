25df111dc5ab6218aa32eac130973d5f9b891f1f
telpuk <telpuk.sergey@gmail.com>
2014-07-15 22:20:10 +0300

    все работает но без объектов

--- js/ppdscript.js
+++ js/ppdscript.js
@@ -1,7 +1,5 @@
 var doc = document,
-    activeQuestionIndex,
-    rightQuestions = [],
-    wrongQuestions = [],
+    activeQuestionIndex = indexActiveTest = rightQuestions = wrongQuestions = countAnsweredQuestion = 0, img = null,
     listTestName = doc.getElementsByClassName('namesTest')[0],
     hidden = doc.getElementsByClassName('hidden')[0],
     testList = doc.getElementsByClassName('testList')[0],
@@ -12,32 +10,40 @@ var doc = document,
     countWrongQuestions = doc.getElementsByClassName('wrong')[0],
     contentQuestions = doc.getElementsByClassName('content')[0],
     imgQuestions = doc.getElementsByClassName('imgQuestions')[0],
+    button = doc.getElementsByClassName('button')[0],
     listAnswers = doc.getElementsByClassName('listAnswers')[0],
+    answerButton = doc.getElementsByClassName('answerButton')[0],
     skipAnswerButton = doc.getElementsByClassName('skipAnswerButton')[0];
 
-var displayShowRightQuestions = function(count) {
-    countRightQuestions.innerText = count.length;
+var displayShowRightQuestions = function(text) {
+    countRightQuestions.innerText = text;
 };
 
-var displayShowWrongQuestions = function(count) {
-    countWrongQuestions.innerText = count.length;
+var displayShowWrongQuestions = function(text) {
+    countWrongQuestions.innerText = text;
+};
+
+var createContentList = function(li, id, listAnswers) {
+    var input = doc.createElement('INPUT');
+    var textAnswers = doc.createTextNode(listAnswers[id]);
+    input.setAttribute('type', 'radio');
+    input.setAttribute('name', 'answer');
+    input.setAttribute('value', id);
+    //устанавливаем default
+    if (parseInt(id, 10) === 0) {
+        input.setAttribute('checked', 'true');
+    }
+    li.appendChild(textAnswers);
+    li.appendChild(input);
+    li.appendChild(textAnswers);
+    return li;
 };
 
 var createLisAnswers = function(listAnswers) {
     var ul = doc.createElement('UL');
     for (var id in listAnswers) {
         var li = doc.createElement('LI');
-        var input = doc.createElement('INPUT');
-        var textAnswers = doc.createTextNode(listAnswers[id]);
-        input.setAttribute('type', 'radio');
-        input.setAttribute('name', 'answer');
-        input.setAttribute('value', id);
-        //устанавливаем default
-        if (parseInt(id, 10) === 0) {
-            input.setAttribute('checked', 'true');
-        }
-        li.appendChild(input);
-        li.appendChild(textAnswers);
+        li = createContentList(li, id, listAnswers);
         ul.appendChild(li);
     }
     return ul;
@@ -48,12 +54,17 @@ var buildQuestion = function(indexActive) {
     activeQuestions.innerText = activeQuestionIndex + 1;
     contentQuestions.innerText = quizData[indexActiveTest].questions[activeQuestionIndex].question;
 
-    if (quizData[indexActiveTest].questions[activeQuestionIndex].questionImg) {
-        imgQuestions.appendChild(function(img) {
-            var imgtest = doc.createElement('IMG');
-            imgtest.src = img;
-            return imgtest;
-        }(quizData[indexActiveTest].questions[activeQuestionIndex].questionImg));
+    if (img === null && quizData[indexActiveTest].questions[activeQuestionIndex].questionImg) {
+        img = doc.createElement('IMG');
+        img.src = quizData[indexActiveTest].questions[activeQuestionIndex].questionImg;
+        imgQuestions.appendChild(img);
+    } else if (quizData[indexActiveTest].questions[activeQuestionIndex].questionImg) {
+        img.src = quizData[indexActiveTest].questions[activeQuestionIndex].questionImg;
+        if (img.className === 'hidden') {
+            removeClass(img, 'hidden');
+        }
+    } else if (!quizData[indexActiveTest].questions[activeQuestionIndex].questionImg && img) {
+        addClass(img, 'hidden');
     }
 
     listAnswers.appendChild(createLisAnswers(quizData[indexActiveTest].questions[activeQuestionIndex].answers));
@@ -63,80 +74,75 @@ var buildQuestion = function(indexActive) {
 var buildTestWidget = function(indexActive) {
     //установить активный тест
     indexActiveTest = indexActive;
-    titleTest.innerText = quizData[indexActive].title;
-    question.innerText = quizData[indexActive].questions.length;
+    titleTest.innerText = quizData[indexActiveTest].title;
+    question.innerText = quizData[indexActiveTest].questions.length;
     displayShowRightQuestions(rightQuestions);
     displayShowWrongQuestions(wrongQuestions);
-    buildQuestion(0);
 };
 
-var repleceClass = function(element, nameClass) {
-    element.className = nameClass;
+var addClass = function(element, nameClass) {
+    element.classList.add(nameClass);
 };
-
-var romeveClass = function(element, nameClass) {
+var removeClass = function(element, nameClass) {
     element.classList.remove(nameClass);
 };
 
+var ListTestEvent = function(e) {
+    var event = e || window.event;
+    var target = event.target || event.srcElement;
+    var linkActiv = listTestName.getElementsByTagName('A');
+    for (var id in linkActiv) {
+        if (linkActiv[id] === target) {
+            buildTestWidget(id);
+            buildQuestion(0);
+        }
+    }
+    testList.className = 'hidden';
+    removeClass(hidden, 'hidden');
+};
+
+var buttonTestEvent = function(e) {
+    var event = e || window.event;
+    var target = event.target || event.srcElement;
+    if (target.className === 'answerButton') {
+        clikNextButton();
+    } else if (target.className === 'skipAnswerButton') {
+        clickSkipButton();
+    }
+};
 //создание элементов списка
 var createListTest = function() {
     var ul = doc.createElement('UL');
     listTestName.appendChild(ul);
+    ul.addEventListener('click', ListTestEvent);
+    button.addEventListener('click', buttonTestEvent);
     for (var testId in quizData) {
         var text = doc.createTextNode(quizData[testId].title);
         var li = doc.createElement('LI');
         var a = doc.createElement('A');
         a.setAttribute("href", '#');
-
-        a.addEventListener('click', function(index) {
-            return function() {
-                buildTestWidget(index);
-            };
-        }(testId));
-
-        a.addEventListener('click', function() {
-            repleceClass(testList, 'hidden');
-        });
-        a.addEventListener('click', function() {
-            repleceClass(hidden, 'hiddenShowDisplay');
-        });
         a.appendChild(text);
         li.appendChild(a);
         ul.appendChild(li);
     }
-
 };
 
-var deleteQuestion = function() {
-    contentQuestions.removeChild(contentQuestions.firstChild);
-    //проверка на картику
-    if (imgQuestions.firstChild) {
-        imgQuestions.removeChild(imgQuestions.firstChild);
-    }
-
+var deleteOptionsQuestion = function() {
     listAnswers.removeChild(listAnswers.firstChild);
-
 };
 
 var getIndex = function(idx) {
-    var countFlag = 0;
     var countQuestions = quizData[indexActiveTest].questions.length;
 
-    for (var id in quizData[indexActiveTest].questions) {
-        if (quizData[indexActiveTest].questions[id].flag) {
-            ++countFlag;
-        }
-    }
-
-    if (countFlag === countQuestions) {
-        skipAnswerButton.style.display = 'none';
+    if (countAnsweredQuestion === countQuestions) {
+        addClass(skipAnswerButton, 'hidden');
         return false;
     } else {
-        if (countFlag === countQuestions - 1) {
-            skipAnswerButton.style.display = 'none';
+        if (countAnsweredQuestion === countQuestions - 1) {
+            addClass(skipAnswerButton, 'hidden');
         }
         idx = ++idx > (countQuestions - 1) ? 0 : idx;
-        while (quizData[indexActiveTest].questions[idx].flag) {
+        while (quizData[indexActiveTest].questions[idx].answeredQuestion) {
             idx = ++idx > (countQuestions - 1) ? 0 : idx;
         }
         return idx;
@@ -145,10 +151,9 @@ var getIndex = function(idx) {
 };
 
 var resetFlagsANDanswers = function() {
-    rightQuestions = [];
-    wrongQuestions = [];
+    activeQuestionIndex = indexActiveTest = rightQuestions = wrongQuestions = countAnsweredQuestion = 0;
     for (var id in quizData[indexActiveTest].questions) {
-        quizData[indexActiveTest].questions[id].flag = false;
+        quizData[indexActiveTest].questions[id].answeredQuestion = false;
     }
 };
 var setFlagPassedTest = function() {
@@ -163,43 +168,41 @@ var clikNextButton = function() {
         if (inputListRadio[idInput].checked) {
             var answer = parseInt(inputListRadio[idInput].value, 10);
             if ((++answer) === parseInt(quizData[indexActiveTest].questions[activeQuestionIndex].right, 10)) {
-                rightQuestions.push(1);
-                displayShowRightQuestions(rightQuestions);
+                displayShowRightQuestions(++rightQuestions);
             } else {
-                wrongQuestions.push(1);
-                displayShowWrongQuestions(wrongQuestions);
+                displayShowWrongQuestions(++wrongQuestions);
                 alert(quizData[indexActiveTest].questions[activeQuestionIndex].wrongAnswersComment);
             }
         }
     }
-    quizData[indexActiveTest].questions[activeQuestionIndex].flag = true;
+    quizData[indexActiveTest].questions[activeQuestionIndex].answeredQuestion = true;
+    ++countAnsweredQuestion;
     var id = getIndex(activeQuestionIndex);
-    if (id >= 0 && id !== false) {
-        deleteQuestion();
+    if (id !== false) {
+        deleteOptionsQuestion();
         buildQuestion(id);
     } else {
         alert('Статистика');
-        repleceClass(doc.getElementsByClassName('hiddenShowDisplay')[0], 'hidden');
-        repleceClass(doc.getElementsByClassName('hidden')[0], 'hiddenShowDisplay');
-        skipAnswerButton.style.display = '';
-        deleteQuestion();
-        resetFlagsANDanswers();
+        hidden.className = 'hidden';
+        removeClass(testList, 'hidden');
+        removeClass(skipAnswerButton, 'hidden');
+        deleteOptionsQuestion();
         setFlagPassedTest();
-
+        resetFlagsANDanswers();
     }
 };
 
-var clikSkipButton = function() {
+var clickSkipButton = function() {
     var id = getIndex(activeQuestionIndex);
-    deleteQuestion();
+    deleteOptionsQuestion();
     buildQuestion(id);
-
 };
+//добавляем элемент в нашу глобальную переменную
 (function() {
     for (var idQuest in quizData) {
         var idQuizData = quizData[idQuest].questions;
         for (var i in idQuizData) {
-            idQuizData[i].flag = false;
+            idQuizData[i].answeredQuestion = false;
             idQuizData[i].wrongAnswersComment = "Не правильно отвечен вопрос!";
         }
     }
@@ -207,15 +210,4 @@ var clikSkipButton = function() {
 
 (function() {
     createListTest();
-    doc.getElementsByClassName('answerButton')[0].addEventListener('click', clikNextButton);
-    doc.getElementsByClassName('skipAnswerButton')[0].addEventListener('click', clikSkipButton);
 })();
-
-
-
-
-
-
-
-
-
